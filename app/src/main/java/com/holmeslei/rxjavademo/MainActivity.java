2d24ea6a02852e577115846705d330c53b1e88f3
LeiHolmes <xulei964270141@qq.com>
2017-07-03 17:27:49 +0800

    rx基本实现

--- app/src/main/java/com/holmeslei/rxjavademo/MainActivity.java
+++ app/src/main/java/com/holmeslei/rxjavademo/MainActivity.java
@@ -2,6 +2,11 @@ package com.holmeslei.rxjavademo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initObserver();
+    }
+
+    private void initObserver() {
+        //step1 创建观察者Observer
+        Observer observer = new Observer() {
+            @Override
+            public void onCompleted() { //不再有新的事件发出时回调
+                Log.e("rx_test", "onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) { //事件处理出现异常时回调
+                Log.e("rx_test", "onError");
+            }
+
+            @Override
+            public void onNext(Object o) { //相当于普通观察者模式的update()
+                Log.e("rx_test", "onNext:" + o.toString());
+            }
+        };
+
+        //step2 创建被观察者Observable
+        //第一种方式
+        Observable<Object> observable1 = Observable.create(new Observable.OnSubscribe<Object>() {
+
+            @Override
+            public void call(Subscriber<? super Object> subscriber) {
+                for (int i = 0; i < 5; i++) {
+                    subscriber.onNext("xulei" + i);
+                }
+                subscriber.onCompleted();
+            }
+        });
+        //第二种方式
+        Observable observable2 = Observable.just("one", "two", "three");
+        //第三种方式
+        String[] parameters = {"one", "two", "three"};
+        Observable observable3 = Observable.from(parameters);
+
+        //step3 被观察者Observable订阅观察者Observer
+        observable1.subscribe(observer);
     }
 }
