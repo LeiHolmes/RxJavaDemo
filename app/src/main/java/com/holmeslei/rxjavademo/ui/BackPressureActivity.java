3ff336b0d9ada69080fd1841919300585e4c253f
xulei <xulei964270141@qq.com>
2017-08-03 10:50:54 +0800

    不支持背压operator实现流速控制

--- app/src/main/java/com/holmeslei/rxjavademo/ui/BackPressureActivity.java
+++ app/src/main/java/com/holmeslei/rxjavademo/ui/BackPressureActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import com.holmeslei.rxjavademo.R;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
@@ -28,8 +29,10 @@ public class BackPressureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_back_pressure);
 //        simulateBackPressure();
-        reactivePull();
-        controlByFilter();
+//        reactivePull();
+//        controlByFilter();
+//        controlByCache();
+//        controlBySpecialOperator();
     }
 
     /**
@@ -99,12 +102,74 @@ public class BackPressureActivity extends AppCompatActivity {
     /**
      * 不支持背压的Observevable做流速控制
      * 通过过滤抛弃大部分事件
+     * 可使用sample，throttleFirst等操作符
      */
     private void controlByFilter() {
-        //使用sample过滤操作符，每隔200ms取里时间点最近的事件
+        //使用sample过滤操作符，每隔200ms取里时间点最近的事件发送
         Observable.interval(1, TimeUnit.MILLISECONDS)
                 .observeOn(Schedulers.newThread())
                 .sample(200, TimeUnit.MILLISECONDS)
                 .subscribe((Long aLong) -> Log.e("rx_test", "controlByFilter：sample：" + aLong));
+    }
+
+    /**
+     * 通过缓存一部分被观察者发来的数据再读来控制流速
+     * 可使用buffer，window等操作符
+     */
+    private void controlByCache() {
+        //使用buffer过滤操作符，将100ms内的事件打包为list发送
+        Observable.interval(1, TimeUnit.MILLISECONDS)
+                .observeOn(Schedulers.newThread())
+                .buffer(100, TimeUnit.MILLISECONDS)
+                .subscribe((longs) -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Log.e("rx_test", "controlByCache：buffer：" + longs.size());
+                });
+    }
+
+    /**
+     * 通过两种特殊操作符来控制流速
+     * onBackpressurebuffer：把observable发送出来的事件做缓存，当request方法被调用的时候，给下层流发送一个item(如果给这个缓存区设置了大小，那么超过了这个大小就会抛出异常)。
+     * onBackpressureDrop：将observable发送的事件抛弃掉，直到subscriber再次调用request(n)方法的时候，再发送这之后的n个事件。
+     */
+    private void controlBySpecialOperator() {
+        //使用onBackpressureDrop可是不支持背压的操作符也可响应下游观察者的request(n)
+        Observable.interval(1, TimeUnit.MILLISECONDS)
+                .onBackpressureDrop()
+                .observeOn(Schedulers.newThread())
+                .subscribe(new Subscriber<Long>() {
+                    @Override
+                    public void onStart() {
+                        super.onStart();
+                        Log.e("rx_test", "controlBySpecialOperator：" + "onStart");
+                        request(1);
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        Log.e("rx_test", "controlBySpecialOperator：" + "onCompleted");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        Log.e("rx_test", "controlBySpecialOperator：" + aLong);
+                        try {
+                            Thread.sleep(500);
+                            request(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        //出现0-15这样连贯的数据，是因为observeOn操作符内部有一个长度为16的缓存区，它会先将请求前16个事件缓存起来
     }
 }
